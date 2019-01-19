387c53668a7b627fd8b0a8160a2ab289e89224b9
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-01-18 20:43:00 -0500

    Added yargs to support command line argumenets

--- ls.js
+++ ls.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 const filesize = require('filesize');
+const argv = require('yargs').argv;
+
+console.log(argv.o);
+
+console.log(argv.parallel);
 
 var files = fs.readdirSync('./');
 files.forEach(file => {
