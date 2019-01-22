5baf3608def974d9f11e10469d6b3c80c65fa55d
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-01-22 16:28:15 -0500

    Made chunk size and number of chunks configurable

--- multiGet.js
+++ multiGet.js
@@ -3,43 +3,52 @@ const fs = require('fs');
 
 let fileName = process.argv[3] || 'file.txt'; // Get file name from command line arguments 
 let url = process.argv[5]; // Get http url from command line arguments 
-let numberofChunks = 4; // Default number of requests need to be made to the server
-const CHUNK_SIZE = 1048576 // 1 MiB = 1024 KB * 1024 B
+let numberofChunks = process.argv[9] || 4; // Default number of requests need to be made to the server
+const CHUNK_SIZE = process.argv[7] * 1048576 || 1048576; // 1 MiB = 1024 KB * 1024 B
 
 let downloadFile = async function() { // async-await 
-    if(process.argv.length < 6 || process.argv.length > 6) {
-        console.info(`Usage: `+process.argv[0]+` [OPTIONS] url \n\t -output string \n\t\tWrite output to <file> instead of default \n\t -url \n\t\tDownload chunks in parallel instead of sequentially`);
+    if(process.argv.length < 6 || process.argv.length > 10) {
+        console.info(`Usage: ${process.argv[0]} [OPTIONS] url \n\t -output string \n\t\tWrite output to <file> instead of default \n\t -url \n\t\tDownload chunks in parallel instead of sequentially`);
     }
     else {
-        console.log("Downloading first 4 chunks of "+url+" to "+fileName);
+        console.log(`Downloading first 4 chunks of ${url} to ${fileName}`);
         for(let i=0; i<numberofChunks; i++) { // Seriel execution
             const requestConfig = {
                 headers: {
-                    Range: `bytes=`+(i*CHUNK_SIZE)+`-`+((i+1)*CHUNK_SIZE-1) // Range headers Eg: Range: bytes=0-1048576
+                    Range: `bytes=${i*CHUNK_SIZE}-${(i+1)*CHUNK_SIZE-1}` // Range headers Eg: Range: bytes=0-1048576
                 },
-                responseType: `arraybuffer` // arraybuffer for blob data type
+                responseType: 'arraybuffer' // arraybuffer for blob data type
             }
 
             // console.log(requestConfig);
             
-            await axios.get(url, requestConfig).then((response) => { // Perform axios get call using the above request config
-                // console.log(response.headers);
-                let blob = response.data;
-                let offset = 0;
-                let chunkLength = (i+1)*CHUNK_SIZE - (i*CHUNK_SIZE);
-                let position = i*CHUNK_SIZE;
-                fs.open(fileName, 'w', function(err, fd) { 
-                    if (err) {
-                        throw 'could not open file: ' + err;
-                    }
-                    fs.write(fd, blob, offset, chunkLength, position, function(err) { // write blob file in 1 MiB chunks serially 
-                        if (err) throw 'error writing file: ' + err;
-                        fs.close(fd, function() {
-                            console.log('Downloaded '+(i+1)+' MiB chunk successfully');
+            await axios.get(url, requestConfig)
+                .then((response) => { // Perform axios get call using the above request config
+                    // console.log(response.headers);
+                    let blob = response.data;
+                    let offset = 0;
+                    let chunkLength = (i+1)*CHUNK_SIZE - (i*CHUNK_SIZE);
+                    let position = i*CHUNK_SIZE;
+                    fs.open(fileName, 'w', function(err, fd) { 
+                        if (err) {
+                            throw `Could not open file: ${err}`;
+                        }
+                        fs.write(fd, blob, offset, chunkLength, position, function(err) { // write blob file in 1 MiB chunks serially 
+                            if (err) throw `Error writing file: ${err}`;
+                            fs.close(fd, function() {
+                                console.log(`Downloaded ${i+1} MiB chunk successfully`);
+                            });
                         });
                     });
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    }
                 });
-            });
+
         }
     }
 }
